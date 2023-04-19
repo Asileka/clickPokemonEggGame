d505a8ea9c23ba95b1bb11dcaa4612a19b5bc9db
Asileka <asilkatya@gmail.com>
2023-04-19 16:52:21 +0100

    added basic functional app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import togepipic from "./images/250px-0175Togepi.png";
+import togeticpic from "./images/176.png";
+import togekiss from "./images/468.png";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [clicks, setClicks] = useState(0);
+  const [pokemonPic, setPokemonPic] = useState(togepipic);
+  const [time, setTime] = useState("?");
+  const [startTime, setStartTime] = useState(0);
+
+  function pressTogepi() {
+    const newclick = clicks + 1;
+    setClicks(newclick);
+    if (newclick === 1) {
+      setStartTime(Date.now() / 1000);
+    }
+    if (newclick >= 10 && newclick < 20) {
+      setPokemonPic(togeticpic);
+    }
+    if (newclick === 20) {
+      setPokemonPic(togekiss);
+      setTime(Date.now() / 1000 - startTime);
+    }
+  }
+  function resetGame() {
+    setClicks(0);
+    setPokemonPic(togepipic);
+    setTime("?");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <p>Egg Game</p>
+        <img src={pokemonPic} alt="togepi" onClick={pressTogepi} />
+        <p>Clicks: {clicks}</p>
+        <p>Your time: {time}</p>
+        <button onClick={resetGame}>Reset</button>
+      </div>
     </div>
   );
 }
